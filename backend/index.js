d7db08611f2a4a515bf51a85b2a7663ded0c42f6
BASHISTHANAYAK <bashisthanayak09@gmail.com>
2024-01-02 20:59:07 +0530

    update 0.0.3

--- backend/index.js
+++ backend/index.js
@@ -8,7 +8,17 @@ dotenv.config();
 app.use(express.json({ limit: "50mb" }));
 app.use(cors());
 app.use(router);
-
+//to get details from frontend
+app.use(
+    cors({
+      origin: [
+        "*",
+        "http://localhost:3000",
+      ],
+      methods: ["GET", "HEAD", "PUT", "PATCH", "POST", "DELETE"],
+      credentials: true,
+    })
+  );
 const PORT = process.env.PORT;
 
 app.listen(PORT, () => console.log(` app running on port ${PORT}!`));
