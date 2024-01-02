eaece8c4172a3f5e0aac71adf0a9cccf3f00d793
BASHISTHANAYAK <bashisthanayak09@gmail.com>
2024-01-02 22:45:46 +0530

    backend and frontend link added

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,6 +1,7 @@
-const serverUrl ="http://localhost:7000"
-// "https://music-cart-backend-xd18.onrender.com"
-// "http://localhost:7000"; 
+const serverUrl = "https://music-cart-backend-xd18.onrender.com";
+// "http://localhost:7000"
+
+// "http://localhost:7000";
 // // Update with your server URL
 
 export default serverUrl;
