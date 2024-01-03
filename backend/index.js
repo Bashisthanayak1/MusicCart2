aa8737615907fae69212d87903191e5f3d853168
BASHISTHANAYAK <bashisthanayak09@gmail.com>
2024-01-03 16:33:43 +0530

    added project

--- backend/index.js
+++ backend/index.js
@@ -4,17 +4,9 @@ import router from "./Routes/Routes.js";
 import cors from "cors";
 
 const app = express();
+app.use(cors({ origin: "*" }));
 dotenv.config();
 app.use(express.json({ limit: "50mb" }));
-// app.use(cors());
-//to get details from frontend
-app.use(
-  cors({
-    origin: ["*"],
-    methods: ["GET", "HEAD", "PUT", "PATCH", "POST", "DELETE"],
-    credentials: true,
-  })
-);
 app.use(router);
 const PORT = process.env.PORT || 7000;
 
--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,8 +1,5 @@
-const serverUrl = "http://localhost:7000"
+const serverUrl = "http://localhost:7000https://fakemusic-repo-backend.onrender.com"
 // "https://music-cart-backend-xd18.onrender.com";
 // "http://localhost:7000"
-
-// "http://localhost:7000";
-// // Update with your server URL
 
 export default serverUrl;
